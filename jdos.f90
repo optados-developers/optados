30f4f72b509eb64c0bb4864c82e52fd45d1c0524
Jonathan Yates <jonathan.yates@materials.ox.ac.uk>
2011-05-19 18:05:03 +0000

    mpi i/o fix

--- jdos.f90
+++ jdos.f90
@@ -11,36 +11,36 @@ module od_jdos
   contains
 
 !===============================================================================  
-  subroutine jdos_calculate
-  !===============================================================================
-   use od_jdos_utils, only : jdos_utils_calculate,E,jdos_fixed &
-   &,jdos_adaptive, jdos_linear
-   use od_parameters, only : fixed, adaptive, linear
-   use od_io, only : io_time,stdout
-   use od_comms, only : on_root
-   implicit none
-   
-   real(dp) :: time0,time1
-   call jdos_utils_calculate
+    subroutine jdos_calculate
+      !===============================================================================
+      use od_jdos_utils, only : jdos_utils_calculate,E,jdos_fixed &
+           &,jdos_adaptive, jdos_linear
+      use od_parameters, only : fixed, adaptive, linear
+      use od_io, only : io_time,stdout
+      use od_comms, only : on_root
+      implicit none
 
-!-------------------------------------------------------------------------------
-! W R I T E   O U T   D O S  
+      real(dp) :: time0,time1
+      call jdos_utils_calculate
 
-time0=io_time()
-! Otherwise we have written to wdos and dos, so they can be called 
-! by whatever.
-  if(on_root) then
-    if(fixed)    call write_jdos(E, jdos_fixed,  "fixed")
-    if(adaptive) call write_jdos(E, jdos_adaptive, "adaptive")
-    if(linear)   call write_jdos(E, jdos_linear,  "linear")
-    !if(quad)    call write_jdos(E, dos_quad, intdos_quad, "quad")
-   endif
-time1=io_time()
-write(stdout,'(1x,a40,f11.3,a)') 'Time to write jdos to disk ',time1-time0,' (sec)'
+      !-------------------------------------------------------------------------------
+      ! W R I T E   O U T   D O S  
 
-!-------------------------------------------------------------------------------
+      time0=io_time()
+      ! Otherwise we have written to wdos and dos, so they can be called 
+      ! by whatever.
+      if(on_root) then
+         if(fixed)    call write_jdos(E, jdos_fixed,  "fixed")
+         if(adaptive) call write_jdos(E, jdos_adaptive, "adaptive")
+         if(linear)   call write_jdos(E, jdos_linear,  "linear")
+         !if(quad)    call write_jdos(E, dos_quad, intdos_quad, "quad")
+      endif
+      time1=io_time()
+      if(on_root) write(stdout,'(1x,a40,f11.3,a)') 'Time to write jdos to disk ',time1-time0,' (sec)'
 
-  end subroutine jdos_calculate
+      !-------------------------------------------------------------------------------
+
+    end subroutine jdos_calculate
 
   !=============================================================================== 
   subroutine write_jdos(E,dos,dos_name)
