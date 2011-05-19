c0d014d0f77dec608f578b044f3fed887de1a83d
Jonathan Yates <jonathan.yates@materials.ox.ac.uk>
2011-05-19 18:07:12 +0000

    spellings

--- jdos.f90
+++ jdos.f90
@@ -3,44 +3,44 @@
 module od_jdos
   use od_constants, only : dp
   implicit none
-  
-    !-------------------------------------------------------------------------------
+
+  !-------------------------------------------------------------------------------
   ! P U B L I C   F U N C T I O N S 
   public :: jdos_calculate
-  
-  contains
 
-!===============================================================================  
-    subroutine jdos_calculate
-      !===============================================================================
-      use od_jdos_utils, only : jdos_utils_calculate,E,jdos_fixed &
-           &,jdos_adaptive, jdos_linear
-      use od_parameters, only : fixed, adaptive, linear
-      use od_io, only : io_time,stdout
-      use od_comms, only : on_root
-      implicit none
+contains
 
-      real(dp) :: time0,time1
-      call jdos_utils_calculate
+  !===============================================================================  
+  subroutine jdos_calculate
+    !===============================================================================
+    use od_jdos_utils, only : jdos_utils_calculate,E,jdos_fixed &
+         &,jdos_adaptive, jdos_linear
+    use od_parameters, only : fixed, adaptive, linear
+    use od_io, only : io_time,stdout
+    use od_comms, only : on_root
+    implicit none
 
-      !-------------------------------------------------------------------------------
-      ! W R I T E   O U T   D O S  
+    real(kind=dp) :: time0,time1
+    call jdos_utils_calculate
 
-      time0=io_time()
-      ! Otherwise we have written to wdos and dos, so they can be called 
-      ! by whatever.
-      if(on_root) then
-         if(fixed)    call write_jdos(E, jdos_fixed,  "fixed")
-         if(adaptive) call write_jdos(E, jdos_adaptive, "adaptive")
-         if(linear)   call write_jdos(E, jdos_linear,  "linear")
-         !if(quad)    call write_jdos(E, dos_quad, intdos_quad, "quad")
-      endif
-      time1=io_time()
-      if(on_root) write(stdout,'(1x,a40,f11.3,a)') 'Time to write jdos to disk ',time1-time0,' (sec)'
+    !-------------------------------------------------------------------------------
+    ! W R I T E   O U T   D O S  
 
-      !-------------------------------------------------------------------------------
+    time0=io_time()
+    ! Otherwise we have written to wdos and dos, so they can be called 
+    ! by whatever.
+    if(on_root) then
+       if(fixed)    call write_jdos(E, jdos_fixed,  "fixed")
+       if(adaptive) call write_jdos(E, jdos_adaptive, "adaptive")
+       if(linear)   call write_jdos(E, jdos_linear,  "linear")
+       !if(quad)    call write_jdos(E, dos_quad, intdos_quad, "quad")
+    endif
+    time1=io_time()
+    if(on_root) write(stdout,'(1x,a40,f11.3,a)') 'Time to write jdos to disk ',time1-time0,' (sec)'
 
-    end subroutine jdos_calculate
+    !-------------------------------------------------------------------------------
+
+  end subroutine jdos_calculate
 
   !=============================================================================== 
   subroutine write_jdos(E,dos,dos_name)
@@ -53,8 +53,8 @@ module od_jdos
     ! and writes out the DOS to disk
     !=============================================================================== 
     implicit none
-    real(dp), intent(in) :: E(jdos_nbins)
-    real(dp), intent(in) :: dos(jdos_nbins,nspins)
+    real(kind=dp), intent(in) :: E(jdos_nbins)
+    real(kind=dp), intent(in) :: dos(jdos_nbins,nspins)
     character(len=*), intent(in) :: dos_name
     integer :: i, dos_file, ierr
     character(len=11) :: cdate
@@ -105,7 +105,7 @@ module od_jdos
     endif
     close(dos_file)
 
-    
+
     if(trim(output_format)=="xmgrace") then
        call write_jdos_xmgrace(dos_name,E,dos)
     elseif(trim(output_format)=="gnuplot") then 
@@ -115,63 +115,63 @@ module od_jdos
     else
        write(stdout,*)  " WARNING: Unknown output format requested, continuing..."
     endif
-  
+
   end subroutine write_jdos
   !===============================================================================
 
   !=============================================================================== 
   subroutine write_jdos_xmgrace(dos_name,E,dos)
     !=============================================================================== 
-   use xmgrace_utils
-   use od_jdos_utils, only : jdos_nbins
-   use od_electronic, only : nspins
-   use od_io,         only : io_file_unit,io_error,seedname 
-   implicit none 
+    use xmgrace_utils
+    use od_jdos_utils, only : jdos_nbins
+    use od_electronic, only : nspins
+    use od_io,         only : io_file_unit,io_error,seedname 
+    implicit none 
 
-   real(dp), intent(in) :: E(jdos_nbins)
-    real(dp), intent(in) :: dos(jdos_nbins,nspins)
-  
-   real(dp) :: min_x, max_x, min_y, max_y
-  
-   integer :: batch_file,ierr
-   character(len=*), intent(in) :: dos_name
+    real(kind=dp), intent(in) :: E(jdos_nbins)
+    real(kind=dp), intent(in) :: dos(jdos_nbins,nspins)
 
-   batch_file=io_file_unit()
-   open(unit=batch_file,file=trim(seedname)//'.j'//trim(dos_name)//'.agr',iostat=ierr)
-   if(ierr.ne.0) call io_error(" ERROR: Cannot open xmgrace batch file in dos: write_jdos_xmgrace")
+    real(kind=dp) :: min_x, max_x, min_y, max_y
 
-   min_x=minval(E)
-   max_x=maxval(E)
-   
-   min_y=0
-   max_y=maxval(dos)
-   if(nspins>1) then
-     min_y=-max_y
-   endif
+    integer :: batch_file,ierr
+    character(len=*), intent(in) :: dos_name
 
-   
+    batch_file=io_file_unit()
+    open(unit=batch_file,file=trim(seedname)//'.j'//trim(dos_name)//'.agr',iostat=ierr)
+    if(ierr.ne.0) call io_error(" ERROR: Cannot open xmgrace batch file in dos: write_jdos_xmgrace")
+
+    min_x=minval(E)
+    max_x=maxval(E)
+
+    min_y=0
+    max_y=maxval(dos)
+    if(nspins>1) then
+       min_y=-max_y
+    endif
 
 
-   call  xmgu_setup(batch_file)
-   call  xmgu_legend(batch_file)
-   call  xmgu_title(batch_file, min_x, max_x, min_y, max_y, "Joint Electronic Density of States")
-   call  xmgu_subtitle(batch_file,"Generated by OptaDOS")
-    
-   call  xmgu_axis(batch_file,"x","Energy eV")
-   call  xmgu_axis(batch_file,"y","JDOS")
 
-   if(nspins>1) then
-      call xmgu_data_header(batch_file,0,1,"up-spin channel")
-      call xmgu_data_header(batch_file,1,2,"down-spin channel")
-      call xmgu_data(batch_file,0,E(:),dos(:,1))
-      call xmgu_data(batch_file,1,E(:),-dos(:,2))
-   else
-      call xmgu_data_header(batch_file,0,1,"Total JDOS")
-      call xmgu_data(batch_file,0,E(:),dos(:,1))  
-   endif
-  
-  
-  close(batch_file)
+
+    call  xmgu_setup(batch_file)
+    call  xmgu_legend(batch_file)
+    call  xmgu_title(batch_file, min_x, max_x, min_y, max_y, "Joint Electronic Density of States")
+    call  xmgu_subtitle(batch_file,"Generated by OptaDOS")
+
+    call  xmgu_axis(batch_file,"x","Energy eV")
+    call  xmgu_axis(batch_file,"y","JDOS")
+
+    if(nspins>1) then
+       call xmgu_data_header(batch_file,0,1,"up-spin channel")
+       call xmgu_data_header(batch_file,1,2,"down-spin channel")
+       call xmgu_data(batch_file,0,E(:),dos(:,1))
+       call xmgu_data(batch_file,1,E(:),-dos(:,2))
+    else
+       call xmgu_data_header(batch_file,0,1,"Total JDOS")
+       call xmgu_data(batch_file,0,E(:),dos(:,1))  
+    endif
+
+
+    close(batch_file)
 
   end subroutine write_jdos_xmgrace
 
