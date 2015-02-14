fba0b87c8256f7df9cb9eb33f5d28e0cca7c9a3d
Andrew Morris <ajm143@gmail.com>
2015-02-14 13:35:20 +0000

    Preparation for 1.2.380 release

--- optados/src/constants.f90
+++ optados/src/constants.f90
@@ -34,8 +34,8 @@ module od_constants
   
   private ! unless otherise stated
 
- character(len=6), parameter, public :: optados_version=" 1.1  "
- character(len=14), parameter, public :: copyright=" (c) 2010-2013"
+ character(len=6), parameter, public :: optados_version=" 1.2 "
+ character(len=14), parameter, public :: copyright=" (c) 2010-2015"
 
  integer, parameter, public :: dp=selected_real_kind(15,300)
 
