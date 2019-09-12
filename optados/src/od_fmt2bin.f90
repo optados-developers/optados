fff42f7fc80be7db9728e11999bc2897b60a1b41
Andrew Morris <ajm255@cam.ac.uk>
2019-09-12 18:19:47 +0100

    Added all the snippets of f90 from before. From now on, I've got to *think*.

--- optados/src/od_fmt2bin.f90
+++ optados/src/od_fmt2bin.f90
@@ -80,8 +80,32 @@ module  od_conv
   subroutine write_ome_bin()
     !=========================================================================
     implicit none
+    
+    integer,parameter:: dp=selected_real_kind(15,300) ! Define double precision
+    real(dp):: file_version=1.0_dp          ! File version
+    character(len=80):: file_header         ! File header comment
+    integer,parameter:: num_kpoints=1                   ! Number of k-points
+    integer,parameter:: num_spins=1                     ! Number of spins
+    integer,parameter:: max_eigenv=1             ! Number of bands included in matrix elements
+    integer :: num_eigenvalues(1:num_spins)   ! Number of eigenvalues per spin channel
+    complex(dp):: optical_mat(max_eigenv,max_eigenv,1:3,1:num_kpoints,num_spins) ! OMEs
+    
+    integer :: ik,is,ib,i,jb, ome_unit=6
+    
     write(*,*) "Write a binary ome file"
-    write(*,*) "We wrote a snippet of code to do this"
+    
+    open(unit=ome_unit, form='unformatted', file="ome.out")
+    
+    write(ome_unit) file_version
+    write(ome_unit) file_header
+    
+    do ik=1,num_kpoints
+       do is=1,num_spins
+          write(ome_unit) (((optical_mat(ib,jb,i,ik,is),ib=1,num_eigenvalues(is)), &
+               &jb=1,num_eigenvalues(is)),i=1,3)
+       end do
+    end do
+   
   end subroutine write_ome_bin
   
   !=========================================================================
@@ -116,9 +140,33 @@ module  od_conv
   subroutine  write_dome_bin()
     !=========================================================================
     implicit none
+
+    integer,parameter:: dp=selected_real_kind(15,300) ! Define double precision
+    real(dp):: file_version=1.0_dp          ! File version
+    character(len=80):: file_header         ! File header comment
+    integer,parameter:: num_kpoints=1                   ! Number of k-points
+    integer,parameter:: num_spins=1                     ! Number of spins
+    integer,parameter:: max_eigenv=1             ! Number of bands included in matrix elements
+    integer :: num_eigenvalues(1:num_spins)   ! Number of eigenvalues per spin channel
+    complex(dp):: optical_mat(max_eigenv,max_eigenv,1:3,1:num_kpoints,num_spins) ! OMEs
+    
+    integer :: ik,is,ib,i,jb, ome_unit=6
+    
+    
     write(*,*) "Write a binary dome file"
-    write(*,*) "We wrote a snippet of code to do this"
-    write(*,*) "Not implemented"
+    
+    write(*,*) "Note this is currently writing out a OME not a DOME."
+    open(unit=ome_unit, form='unformatted', file="ome.out")
+    
+    write(ome_unit) file_version
+    write(ome_unit) file_header
+    
+    do ik=1,num_kpoints
+       do is=1,num_spins
+          write(ome_unit) (((optical_mat(ib,jb,i,ik,is),ib=1,num_eigenvalues(is)), &
+               &jb=1,num_eigenvalues(is)),i=1,3)
+       end do
+    end do
   end subroutine write_dome_bin
   
   !=========================================================================
@@ -148,17 +196,58 @@ module  od_conv
     write(*,*) "Read a binary pdos file"
     call elec_pdos_read()
   end subroutine read_pdos_bin
-                    
+  
   !=========================================================================
   subroutine  write_pdos_bin()
     !=========================================================================
-        implicit none
-        write(*,*) "Write a binary pdos file"
-        write(*,*) "We wrote a snippet of code to do this"
-    write(*,*) "Not implemented"
+    implicit none
+    
+    integer,parameter:: dp=selected_real_kind(15,300) ! Define double precision
+    integer,parameter:: num_kpoints=1           ! Number of k-points
+    integer,parameter:: num_spins=1             ! Number of spins
+    integer,parameter:: num_popn_orb=1          ! Number of LCAO projectors
+    integer,parameter:: max_eigenv=1       ! Number of bands included in matrix elements
+    real(dp):: file_version=1.0_dp  ! File version
+    integer:: species(1:num_popn_orb)! Atomic species associated with each projector
+    integer:: ion(1:num_popn_orb)    ! Ion associated with each projector
+    integer:: am_channel(1:num_popn_orb)     ! Angular momentum channel
+    integer:: num_eigenvalues(1:num_spins)   ! Number of eigenvalues per spin channel
+    real(dp):: kpoint_positions(1:num_kpoints,1:3) ! k_x, k_y, k_z in fractions of BZ
+    real(dp):: pdos_weights(1:num_popn_orb,max_eigenv,num_kpoints,num_spins)!Matrix elements
+    character(len=80):: file_header ! File header comment
+    
+    
+    integer :: nk,ns,nb, pdos_file=6
+    
+    open(unit=pdos_file, form='unformatted', file="pdos.out")
+    
+    write(*,*) "Write a binary pdos file"
+    
+    write(pdos_file) file_header
+    write(pdos_file) file_version
+    write(pdos_file) num_kpoints
+    write(pdos_file) num_spins
+    write(pdos_file) num_popn_orb
+    write(pdos_file) max_eigenv
+    write(pdos_file) species(1:num_popn_orb)
+    write(pdos_file) ion(1:num_popn_orb)
+    write(pdos_file) am_channel(1:num_popn_orb)
+    
+    do nk=1,num_kpoints
+       write(pdos_file) nk, kpoint_positions(nk,:)
+       do ns = 1,num_spins
+          write(pdos_file) ns
+          write(pdos_file) num_eigenvalues(ns)
+          do nb = 1,num_eigenvalues(ns)
+             write(pdos_file) real(pdos_weights(num_popn_orb,nb,nk,ns))
+          end do
+       end do
+    end do
+    
+    
   end subroutine write_pdos_bin
-
-    !=========================================================================
+  
+  !=========================================================================
   ! E L N E S   M A T R I X   E L E M E N T S
   !=========================================================================
   
@@ -190,9 +279,42 @@ module  od_conv
   subroutine  write_elnes_bin()
     !=========================================================================
     implicit none
+    
+    integer,parameter:: dp=selected_real_kind(15,300)
+    integer,parameter:: tot_core_projectors=1  ! Total number of core states included in matrix elements
+    integer,parameter:: max_eigenvalues=1      ! Number of bands included in matrix elements
+    integer,parameter:: num_kpoints=1          ! Number of k-points
+    integer,parameter:: num_spins=1            ! Number of spins
+    integer,parameter:: num_eigenvalues(1:num_spins)=1    ! Number of eigenvalues
+    integer:: species(1:tot_core_projectors) ! Atomic species associated with each projector
+    integer:: ion(1:tot_core_projectors)     ! Ion associated with each projector
+    integer:: n(1:tot_core_projectors)       ! Principal quantum number associated with each projector
+    integer:: lm(1:tot_core_projectors)      ! Angular momentum quantum number associated with each projector
+    real(dp):: elnes_mat(tot_core_projectors,max_eigenvalues,3,num_kpoints,num_spins) ! matrix elements
+    
+    
+    integer :: nk,ns,nb,i,jb, elnes_file=6, orb,indx
+    open(unit=elnes_file, form='unformatted', file="elnes.out")
+    
     write(*,*) "Write a binary elnes file"
-    write(*,*) "We wrote a snippet of code to do this"
-    write(*,*) "Not implemented"
+    
+    write(elnes_file) tot_core_projectors
+    write(elnes_file) max_eigenvalues
+    write(elnes_file) num_kpoints
+    write(elnes_file) num_spins
+    write(elnes_file) species(1:tot_core_projectors)
+    write(elnes_file) ion(1:tot_core_projectors)
+    write(elnes_file) n(1:tot_core_projectors)
+    write(elnes_file) lm(1:tot_core_projectors)
+    
+    do nk = 1,num_kpoints
+       do ns = 1, num_spins
+          write(elnes_file) (((elnes_mat(orb,nb,indx,nk,ns),orb=1,&
+               &tot_core_projectors),nb=1,num_eigenvalues(ns)),indx=1,3)
+       end do
+    end do
+    
+    
   end subroutine write_elnes_bin
   
 
