41a6a3d922c49c3ef77c1c74b1f9627457aa65f4
Andrew J. Morris <andrewjmorris143@gmail.com>
2022-02-24 16:21:48 +0000

    Added a more robust parser for data files. It checks more stuff, and the same benchmark files still work.

--- optados/test-suite/tools/test_parse.py
+++ optados/test-suite/tools/test_parse.py
@@ -2,8 +2,8 @@
 # AJM 8/2/2022
 #
 import sys
-import parsers.parse_od2od_log
+import parsers.parse_od2od_fmt
 
 print(sys.argv)
-out=parsers.parse_od2od_log.parse(sys.argv[1])
+out=parsers.parse_od2od_fmt.parse(sys.argv[1])
 print(out)
