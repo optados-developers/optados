e5031d1df7307d74249b7758d797b456e198c28d
Andrew J. Morris <andrewjmorris143@gmail.com>
2022-02-24 16:21:48 +0000

    Added a more robust parser for data files. It checks more stuff, and the same benchmark files still work.

--- optados/test-suite/tools/parsers/parse_od2od_fmt.py
+++ optados/test-suite/tools/parsers/parse_od2od_fmt.py
@@ -19,17 +19,27 @@ def parse(fname):
         print("[{}.{}] Parsing file '{}'".format(
             __name__, inspect.currentframe().f_code.co_name, fname))
 
+    line_counter=0
     with open(fname) as f:
         for line in f:
-            pass
-        last_line = line
-
-    elements = last_line.split()
-
-
-    retdict["final_element_two"].append(float(elements[1]))
-    if len(elements)>4:
-        retdict["final_element_five"].append(float(elements[4]))
+            # Let's only grab every 10 lines, no point going overboard
+            if (line_counter % 10) == 0:
+                elements = line.split()
+                # If there's only one element it's probably a header so lets ignore it.
+                if len(elements)>1:
+                    try:
+                        # Maybe the header isn't a float if so, let's ignore it, and pick
+                        # it up again when we're out of the header
+                        retdict[str(line_counter)+"final_element_two"].append(float(elements[1]))
+                    except:
+                        pass
+                # Let's try another column too, if it's there.
+                if len(elements)>4:
+                    try:
+                        retdict[str(line_counter)+"final_element_five"].append(float(elements[4]))
+                    except:
+                        pass
+            line_counter=line_counter+1
 
     ###############################################################
 
