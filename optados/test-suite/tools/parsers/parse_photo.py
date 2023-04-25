ada5ab85559eaf1af2b39e5eda5213c58b443ed0
Felix Mildner <felix@wuppersaver.de>
2023-04-25 12:37:20 +0100

    Updated and validated test to work correctly

--- optados/test-suite/tools/parsers/parse_photo.py
+++ optados/test-suite/tools/parsers/parse_photo.py
@@ -13,9 +13,9 @@ e_fermi_fb = re.compile("Fermi\ energy\ \(Fixed\ broadening\)\ \:\s*([0-9\.-]+)\
 e_fermi_ab = re.compile("Fermi\ energy\ \(Adaptive\ broadening\)\ \:\s*([0-9\.-]+)\s*")
 e_fermi_lb = re.compile("Fermi\ energy\ \(Linear\ broadening\)\ \:\s*([0-9\.-]+)\s*")
 
-qe_bulk = re.compile("Bulk\ \s*([0-9\.-]+)\s*")
-qe_total = re.compile("Total\ Quantum\ Efficiency\ \(electrons\/photon\)\:\s*([0-9\.-]+)\s*") 
-mte = re.compile("Weighted\ Mean\ Transverse\ Energy\ \(eV\)\:\s*([0-9\.-]+)\s*")
+qe_bulk = re.compile("Bulk \s*([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\s*")
+qe_total = re.compile("Total Quantum Efficiency \(electrons/photon\):\s+([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\s*") 
+mte = re.compile("Weighted\ Mean\ Transverse\ Energy\ \(eV\)\:\s+([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\s*")
 
 def parse(fname):
     """
@@ -45,22 +45,18 @@ def parse(fname):
         if match:
             retdict["fermi_lb"].append(float(match.groups()[0]))
             continue
-
         match = qe_bulk.search(l)
         if match:
-            retdict['qe_bulk'].append(float(match.groups()[0]))
+            retdict['qe_bulk'].append(float(match.groups()[0]+'E'+match.groups()[1]))
             continue
-
         match = qe_total.search(l)
         if match:
-            retdict['qe_total'].append(float(match.groups()[0]))
+            retdict['qe_total'].append(float(match.groups()[0]+'E'+match.groups()[1]))
             continue
-
         match = mte.search(l)
         if match:
-            retdict['mte'].append(float(match.groups()[0]))
+            retdict['mte'].append(float(match.groups()[0]+'E'+match.groups()[1]))
             continue
-
         ###############################################################
 
 
