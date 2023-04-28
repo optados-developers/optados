cfc61b6bcd5cdee03546b1f6c3a945442059854c
Felix Mildner <felix@wuppersaver.de>
2023-04-28 16:16:15 +0100

    Added testing of faulty input

--- optados/test-suite/tools/parsers/parse_photo_opterr.py
+++ optados/test-suite/tools/parsers/parse_photo_opterr.py
@@ -9,13 +9,7 @@ from collections import defaultdict
 
 from . import show_output
 
-e_fermi_fb = re.compile("Fermi\ energy\ \(Fixed\ broadening\)\ \:\s*([0-9\.-]+)\s*")
-e_fermi_ab = re.compile("Fermi\ energy\ \(Adaptive\ broadening\)\ \:\s*([0-9\.-]+)\s*")
-e_fermi_lb = re.compile("Fermi\ energy\ \(Linear\ broadening\)\ \:\s*([0-9\.-]+)\s*")
-
-qe_bulk = re.compile("Bulk \s*([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\s*")
-qe_total = re.compile("Total Quantum Efficiency \(electrons/photon\):\s+([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\s*") 
-mte = re.compile("Weighted\ Mean\ Transverse\ Energy\ \(eV\)\:\s+([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\s*")
+error_msg = re.compile("Error: ([A-Za-z]+)")
 
 def parse(fname):
     """
@@ -32,30 +26,9 @@ def parse(fname):
 
     for lno, l in enumerate(lines):
 
-        # various Omegas (four numbers, typically at the end)
-        match = e_fermi_fb.search(l)
+        match = error_msg.search(l)
         if match:
-            retdict["fermi_fb"].append(float(match.groups()[0]))
-            continue
-        match = e_fermi_ab.search(l)
-        if match:
-            retdict["fermi_ab"].append(float(match.groups()[0]))
-            continue
-        match = e_fermi_lb.search(l)
-        if match:
-            retdict["fermi_lb"].append(float(match.groups()[0]))
-            continue
-        match = qe_bulk.search(l)
-        if match:
-            retdict['qe_bulk'].append(float(match.groups()[0]+'E'+match.groups()[1]))
-            continue
-        match = qe_total.search(l)
-        if match:
-            retdict['qe_total'].append(float(match.groups()[0]+'E'+match.groups()[1]))
-            continue
-        match = mte.search(l)
-        if match:
-            retdict['mte'].append(float(match.groups()[0]+'E'+match.groups()[1]))
+            retdict['error_msg'].append(match.groups()[0])
             continue
         ###############################################################
 
